e34db31375d7541333a2c2c11e20d526dbf5b172
Leandro Santiago <leandro@lightmeter.io>
2020-03-19 19:22:03 +0100

    WIP: use postfix-log-parser extracted library
    TODO: add it to go.mod

--- main.go
+++ main.go
@@ -2,91 +2,45 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"database/sql"
-	"encoding/gob"
 	"encoding/json"
 	_ "github.com/mattn/go-sqlite3"
+	parser "gitlab.com/lightmeter/postfix-log-parser"
+	"gitlab.com/lightmeter/postfix-log-parser/rawparser"
 	"log"
 	"net/http"
 	"os"
-	"regexp"
 )
 
-const (
-	// NOTE: adapted from https://github.com/youyo/postfix-log-parser.git
-	timeSmtpSentStatusRegexpFormat = `(?P<Time>[A-Za-z]{3}\s\s?[0-9]{1,2} [0-9]{2}:[0-9]{2}:[0-9]{2})`
-	hostSmtpSentStatusRegexpFormat = `(?P<Host>[0-9A-Za-z\.]+)`
-	// TODO: the process name can have more slash separated components, such as: postfix/submission/smtpd
-	processSmtpSentStatusRegexpFormat = `(postfix(-[^/]+)?/(?P<Process>[a-z]+)\[[0-9]{1,5}\])`
-	queueIdSmtpSentStatusRegexpFormat = `(?P<Queue>[0-9A-F]+)`
-
-	procRegexpFormat = `^` + timeSmtpSentStatusRegexpFormat + ` ` + hostSmtpSentStatusRegexpFormat + ` ` + processSmtpSentStatusRegexpFormat + `: `
-
-	anythingExceptCommaRegexpFormat = `[^,]+`
-
-	relayComponentsRegexpFormat = `(?P<RelayName>[^\,[]+)` + `\[(?P<RelayIp>[^\],]+)\]` + `:` + `(?P<RelayPort>[\d]+)`
-
-	messageSentWithStatusSmtpSentStatusRegexpFormat = `(?P<MessageSentWithStatus>` +
-		`to=<(?P<RecipientLocalPart>[^@]+)@(?P<RecipientDomainPart>[^>]+)>` + `, ` +
-		`relay=` + relayComponentsRegexpFormat + `, ` +
-		`delay=(?P<Delay>` + anythingExceptCommaRegexpFormat + `)` + `, ` +
-		`delays=(?P<Delays>` + anythingExceptCommaRegexpFormat + `)` + `, ` +
-		`dsn=(?P<Dsn>` + anythingExceptCommaRegexpFormat + `)` + `, ` +
-		`status=(?P<Status>[a-z]+)` + ` ` +
-		`(?P<ExtraMessage>.*)` +
-		`)`
-
-	possiblePayloads = messageSentWithStatusSmtpSentStatusRegexpFormat
-
-	smtpSentStatusRegexpFormat = `^` + queueIdSmtpSentStatusRegexpFormat + `: ` +
-		`(` + possiblePayloads + `)`
-)
-
-type SmtpSentStatusPublisher interface {
-	Publish(SmtpSentStatus)
+type Publisher interface {
+	Publish(parser.Record)
 	Close()
 }
 
-type ChannelBasedSmtpSentStatusPublisher struct {
-	channel chan []byte
+type ChannelBasedPublisher struct {
+	channel chan parser.Record
 }
 
-func (pub *ChannelBasedSmtpSentStatusPublisher) Publish(status SmtpSentStatus) {
-	// FIXME: obviously allocating new buffers and encoders on every message is bad for performance
-	// but this is only testing code
-
-	var encoderBuffer bytes.Buffer
-	encoder := gob.NewEncoder(&encoderBuffer)
-
-	if err := encoder.Encode(&status); err != nil {
-		log.Fatal(err)
-	}
-
-	pub.channel <- encoderBuffer.Bytes()
+func (pub *ChannelBasedPublisher) Publish(status parser.Record) {
+	pub.channel <- status
 }
 
-func (pub *ChannelBasedSmtpSentStatusPublisher) Close() {
+func (pub *ChannelBasedPublisher) Close() {
 	close(pub.channel)
 }
 
-func fillDatabase(db *sql.DB, c chan []byte) {
+func fillDatabase(db *sql.DB, c chan parser.Record) {
 	stmt, err := db.Prepare("insert into smtp(recipient_local_part, recipient_domain_part, relay_name, status) values(?, ?, ?, ?)")
 
 	if err != nil {
 		log.Fatal("error preparing insert statement")
 	}
 
-	for m := range c {
-		// FIXME: obviously allocating new buffers and encoders on every message is bad for performance
-		// but this is only testing code
+	for r := range c {
+		status, cast := r.Payload.(parser.SmtpSentStatus)
 
-		buffer := bytes.NewBuffer(m)
-		decoder := gob.NewDecoder(buffer)
-		var status SmtpSentStatus
-
-		if err := decoder.Decode(&status); err != nil {
-			log.Fatal(err)
+		if !cast {
+			continue
 		}
 
 		_, err := stmt.Exec(status.RecipientLocalPart, status.RecipientDomainPart, status.RelayName, status.Status)
@@ -98,8 +52,8 @@ func fillDatabase(db *sql.DB, c chan []byte) {
 }
 
 func main() {
-	c := make(chan []byte, 10)
-	pub := ChannelBasedSmtpSentStatusPublisher{c}
+	c := make(chan parser.Record, 10)
+	pub := ChannelBasedPublisher{c}
 	go parseLogsFromStdin(&pub)
 
 	os.Remove("lm.db")
@@ -118,8 +72,8 @@ func main() {
 
 	go fillDatabase(db, c)
 
-	countByStatus := func(status string) int {
-		stmt, err := db.Prepare(`select count(status) from smtp where cast(status as text) = ?`)
+	countByStatus := func(status parser.SmtpStatus) int {
+		stmt, err := db.Prepare(`select count(status) from smtp where status = ?`)
 
 		if err != nil {
 			log.Fatal("error preparing query")
@@ -181,12 +135,12 @@ func main() {
 		}
 
 		for query.Next() {
-			var status string
+			var status parser.SmtpStatus
 			var value float64
 
 			query.Scan(&status, &value)
 
-			r = append(r, deliveryValue{status, value * 100})
+			r = append(r, deliveryValue{status.HumanForm(), value * 100})
 		}
 
 		return r
@@ -199,7 +153,7 @@ func main() {
 	}
 
 	http.HandleFunc("/countByStatus", func(w http.ResponseWriter, r *http.Request) {
-		serveJson(w, r, map[string]int{"sent": countByStatus("sent"), "deferred": countByStatus("deferred"), "bounced": countByStatus("bounced")})
+		serveJson(w, r, map[string]int{"sent": countByStatus(parser.SentStatus), "deferred": countByStatus(parser.DeferredStatus), "bounced": countByStatus(parser.BouncedStatus)})
 	})
 
 	http.HandleFunc("/topBusiestDomains", func(w http.ResponseWriter, r *http.Request) {
@@ -207,11 +161,13 @@ func main() {
 	})
 
 	http.HandleFunc("/topBouncedDomains", func(w http.ResponseWriter, r *http.Request) {
-		serveJson(w, r, listDomainAndCount(`select recipient_domain_part, count(recipient_domain_part) as c from smtp where cast(status as text) = "bounced" group by recipient_domain_part order by c desc limit 20`))
+		query := `select recipient_domain_part, count(recipient_domain_part) as c from smtp where status = ` + parser.BouncedStatus.String() + ` group by recipient_domain_part order by c desc limit 20`
+		serveJson(w, r, listDomainAndCount(query))
 	})
 
 	http.HandleFunc("/topDeferredDomains", func(w http.ResponseWriter, r *http.Request) {
-		serveJson(w, r, listDomainAndCount(`select relay_name, count(relay_name) as c from smtp where cast(status as text) = "deferred" group by relay_name order by c desc limit 20`))
+		query := `select relay_name, count(relay_name) as c from smtp where status = ` + parser.DeferredStatus.String() + ` group by relay_name order by c desc limit 20`
+		serveJson(w, r, listDomainAndCount(query))
 	})
 
 	http.HandleFunc("/deliveryStatus", func(w http.ResponseWriter, r *http.Request) {
@@ -223,59 +179,8 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-type LogHeader struct {
-	Time    []byte
-	Host    []byte
-	Process []byte
-}
-
-type SmtpSentStatus struct {
-	Header              LogHeader
-	Queue               []byte
-	RecipientLocalPart  []byte
-	RecipientDomainPart []byte
-	RelayName           []byte
-	RelayIp             []byte
-	RelayPort           []byte
-	Delay               []byte
-	Delays              []byte
-	Dsn                 []byte
-	Status              []byte
-	ExtraMessage        []byte
-}
-
-func indexForGroup(smtpSentStatusRegexp *regexp.Regexp, name string) int {
-	e := smtpSentStatusRegexp.SubexpNames()
-	for i, v := range e {
-		if v == name {
-			return i
-		}
-	}
-
-	panic("Wrong Group Name!")
-}
-
-func parseLogsFromStdin(publisher SmtpSentStatusPublisher) {
+func parseLogsFromStdin(publisher Publisher) {
 	scanner := bufio.NewScanner(os.Stdin)
-
-	smtpSentStatusRegexp := regexp.MustCompile(smtpSentStatusRegexpFormat)
-	procRegexp := regexp.MustCompile(procRegexpFormat)
-
-	timeIndex := indexForGroup(procRegexp, "Time")
-	hostIndex := indexForGroup(procRegexp, "Host")
-	processIndex := indexForGroup(procRegexp, "Process")
-
-	smtpQueueIndex := indexForGroup(smtpSentStatusRegexp, "Queue")
-	smtpRecipientLocalPartIndex := indexForGroup(smtpSentStatusRegexp, "RecipientLocalPart")
-	smtpRecipientDomainPartIndex := indexForGroup(smtpSentStatusRegexp, "RecipientDomainPart")
-	smtpRelayNameIndex := indexForGroup(smtpSentStatusRegexp, "RelayName")
-	smtpRelayIpIndex := indexForGroup(smtpSentStatusRegexp, "RelayIp")
-	smtpRelayPortIndex := indexForGroup(smtpSentStatusRegexp, "RelayPort")
-	smtpDelayIndex := indexForGroup(smtpSentStatusRegexp, "Delay")
-	smtpDelaysIndex := indexForGroup(smtpSentStatusRegexp, "Delays")
-	smtpDsnIndex := indexForGroup(smtpSentStatusRegexp, "Dsn")
-	smtpStatusIndex := indexForGroup(smtpSentStatusRegexp, "Status")
-	smtpExtraMessageIndex := indexForGroup(smtpSentStatusRegexp, "ExtraMessage")
 
 	for {
 		if !scanner.Scan() {
@@ -284,48 +189,16 @@ func parseLogsFromStdin(publisher SmtpSentStatusPublisher) {
 
 		logLine := scanner.Bytes()
 
-		headerMatches := procRegexp.FindSubmatch(logLine)
+		r, err := parser.Parse(logLine)
 
-		if len(headerMatches) == 0 {
+		if err != nil {
+			if err == rawparser.InvalidHeaderLineError {
+				log.Printf("Invalid Postfix header: \"%s\"", string(logLine))
+			}
 			continue
 		}
 
-		if bytes.Compare(headerMatches[processIndex], []byte("smtp")) != 0 {
-			// TODO: implement support for other processes
-			continue
-		}
-
-		linePayload := logLine[len(headerMatches[0]):]
-
-		payloadMatches := smtpSentStatusRegexp.FindSubmatch(linePayload)
-
-		if len(payloadMatches) == 0 {
-			//fmt.Println("New smtp payload: ", string(linePayload))
-			// TODO: implement other stuff done by the "smtp" process
-			continue
-		}
-
-		s := SmtpSentStatus{
-			Header: LogHeader{
-				Time:    headerMatches[timeIndex],
-				Host:    headerMatches[hostIndex],
-				Process: headerMatches[processIndex],
-			},
-
-			Queue:               payloadMatches[smtpQueueIndex],
-			RecipientLocalPart:  payloadMatches[smtpRecipientLocalPartIndex],
-			RecipientDomainPart: payloadMatches[smtpRecipientDomainPartIndex],
-			RelayName:           payloadMatches[smtpRelayNameIndex],
-			RelayIp:             payloadMatches[smtpRelayIpIndex],
-			RelayPort:           payloadMatches[smtpRelayPortIndex],
-			Delay:               payloadMatches[smtpDelayIndex],
-			Delays:              payloadMatches[smtpDelaysIndex],
-			Dsn:                 payloadMatches[smtpDsnIndex],
-			Status:              payloadMatches[smtpStatusIndex],
-			ExtraMessage:        payloadMatches[smtpExtraMessageIndex],
-		}
-
-		publisher.Publish(s)
+		publisher.Publish(r)
 	}
 
 	publisher.Close()
