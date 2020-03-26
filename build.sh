1a060b4dcb7639cf5b54b62856126aea457a0656
Leandro Santiago <leandro@lightmeter.io>
2020-03-26 14:45:35 +0100

    enable extra options on build

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ set -vx
 
 TAGS="${1:-dev}"
 
+shift 1
+
 if [ "$GOOS" = windows ]; then
   OUTPUT="lightmeter.exe"
 else
@@ -16,7 +18,7 @@ export CGO_ENABLED=1
 go mod download
 
 # go get does not play well with modules :-(
-GO111MODULE=off go get -u github.com/shurcooL/vfsgen
+GO111MODULE=off go get -v -u github.com/shurcooL/vfsgen
 
 go generate -tags="$TAGS" gitlab.com/lightmeter/controlcenter/staticdata
-go build -tags="$TAGS" -o "${OUTPUT}"
+go build -tags="$TAGS" -o "${OUTPUT}" "$@"
