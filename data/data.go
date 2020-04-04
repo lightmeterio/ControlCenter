ff6df7e727ee54d845a200e7f596e8c8c5fbf479
Leandro Santiago <leandro@lightmeter.io>
2020-04-04 17:21:47 +0200

    Move timestamping logs out of the database writer thread to its own thread

--- data/data.go
+++ data/data.go
@@ -2,11 +2,22 @@ package data
 
 import (
 	parser "gitlab.com/lightmeter/postfix-log-parser"
+	"time"
 )
 
+// The parsed value as read from the logs
+// TODO: add the timestamp of when the log was read
+// ir order to improve detecting inconsistences on reading
+// from multiple sources
 type Record struct {
 	Header  parser.Header
 	Payload parser.Payload
+}
+
+// A record with the actual timestamp it'll be used to be inserted in the database
+type TimedRecord struct {
+	Time   time.Time
+	Record Record
 }
 
 type Publisher interface {
