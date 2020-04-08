62d01119b7fab9d94d74b0c022f46ea9e9521398
Leandro Santiago <leandro@lightmeter.io>
2020-04-08 12:38:29 +0200

    On assertion, print filename and line number

--- util/util.go
+++ util/util.go
@@ -1,9 +1,19 @@
 package util
 
-import "log"
+import (
+	"log"
+	"runtime"
+)
 
 func MustSucceed(err error, msg string) {
 	if err != nil {
-		log.Fatal("MustSucceed:", msg, "error:", err)
+		_, file, line, ok := runtime.Caller(1)
+
+		if !ok {
+			line = 0
+			file = `<unknown file>`
+		}
+
+		log.Fatal("FAILED:", file, ":", line, ` (`, msg, `), error: "`, err, `"`)
 	}
 }
