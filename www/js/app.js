2989f116007b27fb71b78b18ff6cf4fca3f38dd9
Sam Tuke <mail@samtuke.com>
2020-04-27 17:41:07 +0200

    Change datepicker formatting; Add fontawesome

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,30 @@
 
 // Enable range datepicker
-$('input[name="daterangepicker"]').daterangepicker();
+$(function() {
+
+    var start = moment().subtract(29, 'days');
+    var end = moment();
+
+    function cb(start, end) {
+        $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+    }
+
+    $('#reportrange').daterangepicker({
+        startDate: start,
+        endDate: end,
+        ranges: {
+           'Today': [moment(), moment()],
+           'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+           'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+           'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+           'This Month': [moment().startOf('month'), moment().endOf('month')],
+           'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        }
+    }, cb);
+
+    cb(start, end);
+
+});
 
 // Graph stuff
 var drawDashboard = function() {
