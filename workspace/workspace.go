718afe3575dc659a440f8734e230af010fc1e9b8
Leandro Santiago <leandro@lightmeter.io>
2020-07-03 13:03:47 +0200

    Expose application main settings as part of the workspace

--- workspace/workspace.go
+++ workspace/workspace.go
@@ -2,19 +2,27 @@ package workspace
 
 import (
 	"os"
+	"path"
 	"time"
 
 	"gitlab.com/lightmeter/controlcenter/auth"
 	"gitlab.com/lightmeter/controlcenter/dashboard"
 	"gitlab.com/lightmeter/controlcenter/data"
+	"gitlab.com/lightmeter/controlcenter/lmsqlite3/dbconn"
 	"gitlab.com/lightmeter/controlcenter/logdb"
+	"gitlab.com/lightmeter/controlcenter/meta"
+	"gitlab.com/lightmeter/controlcenter/newsletter"
+	"gitlab.com/lightmeter/controlcenter/settings"
 	"gitlab.com/lightmeter/controlcenter/util"
 )
 
 type Workspace struct {
-	config data.Config
-	logs   logdb.DB
-	auth   *auth.Auth
+	logs logdb.DB
+	auth *auth.Auth
+
+	metaConnPair dbconn.ConnPair
+	meta         *meta.MetadataHandler
+	settings     *settings.MasterConf
 }
 
 func NewWorkspace(workspaceDirectory string, config data.Config) (Workspace, error) {
@@ -34,11 +42,35 @@ func NewWorkspace(workspaceDirectory string, config data.Config) (Workspace, err
 		return Workspace{}, util.WrapError(err)
 	}
 
+	metadataConnPair, err := dbconn.NewConnPair(path.Join(workspaceDirectory, "master.db"))
+
+	if err != nil {
+		return Workspace{}, util.WrapError(err)
+	}
+
+	m, err := meta.NewMetaDataHandler(metadataConnPair)
+
+	if err != nil {
+		return Workspace{}, util.WrapError(err)
+	}
+
+	settings, err := settings.NewMasterConf(m, newsletter.NewSubscriber("https://phplist.lightmeter.io/lists/?p=asubscribe&id=2"))
+
+	if err != nil {
+		return Workspace{}, util.WrapError(err)
+	}
+
 	return Workspace{
-		config: config,
-		logs:   logDb,
-		auth:   auth,
+		logs:         logDb,
+		auth:         auth,
+		metaConnPair: metadataConnPair,
+		meta:         m,
+		settings:     settings,
 	}, nil
+}
+
+func (ws *Workspace) Settings() *settings.MasterConf {
+	return ws.settings
 }
 
 func (ws *Workspace) Dashboard() (dashboard.Dashboard, error) {
@@ -64,7 +96,27 @@ func (ws *Workspace) Run() <-chan interface{} {
 }
 
 func (ws *Workspace) Close() error {
-	return ws.logs.Close()
+	if err := ws.settings.Close(); err != nil {
+		return util.WrapError(err)
+	}
+
+	if err := ws.meta.Close(); err != nil {
+		return util.WrapError(err)
+	}
+
+	if err := ws.metaConnPair.Close(); err != nil {
+		return util.WrapError(err)
+	}
+
+	if err := ws.auth.Close(); err != nil {
+		return util.WrapError(err)
+	}
+
+	if err := ws.logs.Close(); err != nil {
+		return util.WrapError(err)
+	}
+
+	return nil
 }
 
 func (ws *Workspace) HasLogs() bool {
