9f504c59f2018b6d7ae6f3bd8c9fe818b5ca3b30
Sam Tuke <mail@samtuke.com>
2020-07-03 18:46:30 +0200

    Add password reveal button

--- www/js/app.js
+++ www/js/app.js
@@ -300,3 +300,19 @@ function submitLoginForm() {
     console.log(err)
     })
 }
+
+// password field show / hide
+$(document).ready(function() {
+    $("#show_hide_password a").on('click', function(event) {
+        event.preventDefault();
+        if($('#show_hide_password input').attr("type") == "text"){
+            $('#show_hide_password input').attr('type', 'password');
+            $('#show_hide_password i').addClass( "fa-eye-slash" );
+            $('#show_hide_password i').removeClass( "fa-eye" );
+        }else if($('#show_hide_password input').attr("type") == "password"){
+            $('#show_hide_password input').attr('type', 'text');
+            $('#show_hide_password i').removeClass( "fa-eye-slash" );
+            $('#show_hide_password i').addClass( "fa-eye" );
+        }
+    });
+});
