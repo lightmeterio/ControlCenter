31a2b2b767961c1f95aede99bdccf10812c079ed
Leandro Santiago <leandro@lightmeter.io>
2020-07-22 16:56:41 +0200

    Simplify interface of the time converter utility

--- util.go
+++ util.go
@@ -13,17 +13,25 @@ type TimeConverter struct {
 	newYearNotifier func(newYear int, old Time, new Time)
 }
 
-func NewTimeConverter(initialTime Time,
-	year int,
-	timezone *time.Location,
+func NewTimeConverter(initialTime time.Time,
 	newYearNotifier func(int, Time, Time),
 ) TimeConverter {
 	return TimeConverter{
-		timezone:        timezone,
-		year:            year,
-		lastTime:        initialTime,
+		timezone: initialTime.Location(),
+		year:     initialTime.Year(),
+		lastTime: Time{
+			Month:  initialTime.Month(),
+			Day:    uint8(initialTime.Day()),
+			Hour:   uint8(initialTime.Hour()),
+			Minute: uint8(initialTime.Minute()),
+			Second: uint8(initialTime.Second()),
+		},
 		newYearNotifier: newYearNotifier,
 	}
+}
+
+func DefaultTimeInYear(year int, tz *time.Location) time.Time {
+	return time.Date(year, time.January, 1, 0, 0, 0, 0, tz)
 }
 
 func (this *TimeConverter) Convert(t Time) time.Time {
@@ -35,7 +43,7 @@ func (this *TimeConverter) Convert(t Time) time.Time {
 	// FIXME: this probably won't work for any time before the unix epoch (ts negative),
 	// but who will go back in time and run Postfix?
 	if this.lastTime.Unix(this.year, this.timezone) > t.Unix(this.year, this.timezone) {
-		this.year += 1
+		this.year++
 		this.newYearNotifier(this.year, this.lastTime, t)
 	}
 
