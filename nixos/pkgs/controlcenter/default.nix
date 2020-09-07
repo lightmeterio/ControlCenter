9ac64f3d7e32f36bf9c178611cf916380cc53e73
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2020-09-07 09:32:27 +0200

    Change pkg build process for nix

--- nixos/pkgs/controlcenter/default.nix
+++ nixos/pkgs/controlcenter/default.nix
@@ -1,17 +1,45 @@
-{ stdenv, buildGoPackage, fetchgit}:
+{ stdenv, buildGoModule, fetchgit, fetchhg, fetchbzr, fetchsvn, lib }:
 
-buildGoPackage rec {
+buildGoModule rec {
   pname = "lightmeter";
-  version = "0.0.6-1";
-  rev = "1e5fb1e422a1fd0d6bb54c14396671968366851a";
+  version = "master";
+  rev = "dad4451218d3fb99bc1d3240f66dac030ee8f246";
+
+  deleteVendor = true;
 
   goPackagePath = "gitlab.com/lightmeter/controlcenter";
 
   src = fetchgit {
      inherit rev;
      url = "https://gitlab.com/lightmeter/controlcenter";
-     sha256 = "104hws6p6kbrg361vq5ma19838hvabb5gldibyx55aqbijrg5xw0";
+     sha256 = "1pgkf9zlhkblzdjipp17zmpp9di19hizpkqav6bk9i6ajnmf6am8";
   };
 
-  goDeps = ./deps.nix;
+  vendorSha256= null;
+
+  checkPhase = ''
+  '';
+
+  buildPhase = ''
+  go generate -tags="release" ./staticdata
+  go generate ./domainmapping
+  go generate ./po
+  go install -tags="release"
+  '';
+
+  installPhase =  ''
+      runHook preInstall
+      mkdir -p $out
+      dir="$GOPATH/bin"
+      [ -e "$dir" ] && cp -r $dir $out
+      runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "Lightmeter Control Center, the Open Source mailtech monitoring and management application.";
+    homepage = https://lightmeter.io/about/;
+
+    maintainers = with maintainers; [ donutloop ];
+    platforms = platforms.linux ++ platforms.darwin;
+  };
 }
