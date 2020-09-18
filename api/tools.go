01230be54eff183f764a8960e8b0ae6353f886fd
Leandro Santiago <leandro@lightmeter.io>
2020-09-18 14:14:13 +0200

    Added required tools.go to api/docs

--- api/tools.go
+++ api/tools.go
@@ -3,7 +3,3 @@
 package api
 
 import _ "github.com/swaggo/swag/cmd/swag"
-
-// NOTE: added to prevent `go mod tidy` of removing it as a dependency of swag
-// But this is a ugly hack this package is required during dev builds!
-import _ "github.com/alecthomas/template"
