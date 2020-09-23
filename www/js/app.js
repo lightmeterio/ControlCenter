e73c07bb89d275260e20d692915984eb4669e52f
Leandro Santiago <leandro@lightmeter.io>
2020-09-23 14:37:12 +0200

    Make greeting text translatable

--- www/js/app.js
+++ www/js/app.js
@@ -255,9 +255,6 @@ $(document).ready(function() {
 // create welcome text for homepage
 $(document).ready(function() {
     if($(".greeting h3").length){
-        var dateObj = new Date()
-        // FIXME not translated
-        var weekday = dateObj.toLocaleString("default", { weekday: "long" })
-        $(".greeting h3").html("Happy "+weekday+",");
+        $(".greeting h3").html(greetingText());
     }
 });
