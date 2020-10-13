7b7ed77a9092a2f5133db53a6e628d775b4881a4
Leandro Santiago <leandro@lightmeter.io>
2020-10-13 11:31:52 +0200

    Add optional basename argument to test utility TempDBConnection

--- util/testutil/tempdb.go
+++ util/testutil/tempdb.go
@@ -8,10 +8,21 @@ import (
 
 // TempDBConnection creates new database connection pair in a temporary directory
 // It's responsibility of the caller to close the connection before the directory
-// is removed
-func TempDBConnection() (conn dbconn.ConnPair, removeDir func()) {
+// is removed.
+// basename is an optional value specifying the name for the database file,
+// without extension.
+func TempDBConnection(basename ...string) (conn dbconn.ConnPair, removeDir func()) {
 	dir, removeDir := TempDir()
-	conn, err := dbconn.NewConnPair(path.Join(dir, "database.db"))
+
+	filename := path.Join(dir, func() string {
+		if len(basename) > 0 {
+			return basename[0] + ".db"
+		}
+
+		return "database.db"
+	}())
+
+	conn, err := dbconn.NewConnPair(filename)
 
 	if err != nil {
 		log.Panicln("Error creating temporary database:", err)
