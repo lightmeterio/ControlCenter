5b56012afa1091b8256650ac2b8852908df43828
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2020-10-20 17:47:27 +0200

    Change message of echo

--- code2poutil.sh
+++ code2poutil.sh
@@ -3,7 +3,7 @@ echo "po/en/LC_MESSAGES -> .pot"
 go run tools/code2po/main.go -i www -pot -o po/controlcenter.pot
 for d in po/*; do
   if [ -d "$d/LC_MESSAGES" ] && [ $d != "po/en" ]; then
-    echo "$d/LC_MESSAGES -> .pot"
+    echo "$d/LC_MESSAGES -> .po"
     go run tools/code2po/main.go -i www -o "$d/LC_MESSAGES/controlcenter.po" -ids=true
   fi
 done
