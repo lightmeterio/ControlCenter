ac33f18df6b61da1b0dbcfd5b0eb953342b0dbea
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2020-12-01 15:45:51 +0000

    Vuejs: javascript and css tweaks for vuejs

--- frontend/controlcenter/src/views/login.vue
+++ frontend/controlcenter/src/views/login.vue
@@ -1,54 +1,56 @@
 <template>
   <panel-page>
-    <h2>
-      <!-- prettier-ignore -->
-      <translate>Login</translate>
-    </h2>
-    <div class="field-group">
-      <b-form @submit="onSubmit">
-        <b-form-group>
-          <b-form-input
-            name="email"
-            id="email"
-            v-model="form.email"
-            type="email"
-            required
-            aria-describedby="emailHelp"
-            :placeholder="EmailInputPlaceholder"
-            maxlength="255"
-          ></b-form-input>
-          <b-input-group id="show_hide_password">
+    <div id="login-page">
+      <h2>
+        <!-- prettier-ignore -->
+        <translate>Login</translate>
+      </h2>
+      <div class="field-group">
+        <b-form @submit="onSubmit">
+          <b-form-group>
             <b-form-input
-              name="password"
-              id="password"
-              v-model="form.password"
+              name="email"
+              id="email"
+              v-model="form.email"
+              type="email"
               required
-              aria-describedby="passwordHelp"
-              :placeholder="PasswordInputPlaceholder"
-              type="password"
+              aria-describedby="emailHelp"
+              :placeholder="EmailInputPlaceholder"
               maxlength="255"
             ></b-form-input>
-            <div class="input-group-addon" v-on:click="onTogglePasswordShow">
-              <a href=""><i class="fa fa-eye" aria-hidden="true"></i></a>
-            </div>
-          </b-input-group>
-          <p class="align-right">
-            <small
-              ><a
-                target="_blank"
-                href="https://gitlab.com/lightmeter/controlcenter#password-reset"
+            <b-input-group id="show_hide_password">
+              <b-form-input
+                name="password"
+                id="password"
+                v-model="form.password"
+                required
+                aria-describedby="passwordHelp"
+                :placeholder="PasswordInputPlaceholder"
+                type="password"
+                maxlength="255"
+              ></b-form-input>
+              <div class="input-group-addon" v-on:click="onTogglePasswordShow">
+                <a href=""><i class="fa fa-eye" aria-hidden="true"></i></a>
+              </div>
+            </b-input-group>
+            <p class="align-right">
+              <small
+                ><a
+                  target="_blank"
+                  href="https://gitlab.com/lightmeter/controlcenter#password-reset"
+                >
+                  <!-- prettier-ignore -->
+                  <translate>Forgot password?</translate>
+                </a></small
               >
-                <!-- prettier-ignore -->
-                <translate>Forgot password?</translate>
-              </a></small
-            >
-          </p>
-        </b-form-group>
-        <b-button variant="primary" class="w-100" type="submit">
-          <!-- prettier-ignore -->
-          <translate>Login</translate>
-        </b-button>
-      </b-form>
+            </p>
+          </b-form-group>
+          <b-button variant="primary" class="w-100" type="submit">
+            <!-- prettier-ignore -->
+            <translate>Login</translate>
+          </b-button>
+        </b-form>
+      </div>
     </div>
   </panel-page>
 </template>
@@ -108,4 +110,10 @@ export default {
 };
 </script>
 
-<style lang="less"></style>
+<style lang="less">
+#login-page .btn-primary:hover {
+  color: #fff;
+  background-color: #0069d9;
+  border-color: #0062cc;
+}
+</style>
