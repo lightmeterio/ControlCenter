39afa812a9154191b2b80255bfb6cae285c96fdf
Leandro Santiago <leandro@lightmeter.io>
2021-01-18 12:59:39 +0100

    Add new deliverydb package
    Ref #82

---
+++ deliverydb/delivery_test.go
@@ -0,0 +1,411 @@
+package deliverydb
+
+import (
+	"context"
+	. "github.com/smartystreets/goconvey/convey"
+	"gitlab.com/lightmeter/controlcenter/dashboard"
+	"gitlab.com/lightmeter/controlcenter/data"
+	"gitlab.com/lightmeter/controlcenter/domainmapping"
+	"gitlab.com/lightmeter/controlcenter/lmsqlite3"
+	parser "gitlab.com/lightmeter/controlcenter/pkg/postfix/logparser"
+	"gitlab.com/lightmeter/controlcenter/tracking"
+	"gitlab.com/lightmeter/controlcenter/util/errorutil"
+	"gitlab.com/lightmeter/controlcenter/util/testutil"
+	"log"
+	"testing"
+	"time"
+)
+
+func init() {
+	// NOTE: unfortunately the domain mapping code uses a singleton (to be accessed internally via sqlite)
+	// that outlives all the test cases, so it's more clear for it to be defined globally as well
+	m, err := domainmapping.Mapping(domainmapping.RawList{"grouped": []string{"domaintobegrouped.com", "domaintobegrouped.de"}})
+	errorutil.MustSucceed(err)
+
+	lmsqlite3.Initialize(lmsqlite3.Options{
+		"domain_mapping": &m,
+	})
+}
+
+func TestDatabaseCreation(t *testing.T) {
+	Convey("Creation succeceds", t, func() {
+		ws, clearWs := testutil.TempDir(t)
+		defer clearWs()
+
+		log.Println("database name:", ws)
+
+		Convey("Insert some values", func() {
+			db, err := New(ws)
+			So(err, ShouldBeNil)
+
+			done, cancel := db.Run()
+
+			pub := db.ResultsPublisher()
+
+			{
+				result := tracking.Result{}
+				result[tracking.ConnectionBeginKey] = 1
+				result[tracking.ConnectionEndKey] = 2
+				result[tracking.ConnectionClientHostnameKey] = "client.host"
+				result[tracking.ConnectionClientIPKey] = "127.0.0.1"
+				result[tracking.QueueBeginKey] = 2
+				result[tracking.QueueEndKey] = 3
+				result[tracking.QueueSenderLocalPartKey] = "sender"
+				result[tracking.QueueSenderDomainPartKey] = "sender1.com"
+				result[tracking.QueueOriginalMessageSizeKey] = 41
+				result[tracking.QueueProcessedMessageSizeKey] = 90
+				result[tracking.QueueNRCPTKey] = 5
+				result[tracking.QueueMessageIDKey] = "lala@caca.com"
+				result[tracking.ResultDeliveryTimeKey] = 3
+				result[tracking.ResultRecipientLocalPartKey] = "recipient"
+				result[tracking.ResultRecipientDomainPartKey] = "recipient1.com"
+				result[tracking.ResultOrigRecipientLocalPartKey] = nil
+				result[tracking.ResultOrigRecipientDomainPartKey] = nil
+				result[tracking.ResultDelayKey] = ""
+				result[tracking.ResultDelaySMTPDKey] = 1.0
+				result[tracking.ResultDelayCleanupKey] = 2.0
+				result[tracking.ResultDelayQmgrKey] = 3.0
+				result[tracking.ResultDelaySMTPKey] = 4.0
+				result[tracking.ResultDSNKey] = "2.0.0"
+				result[tracking.ResultStatusKey] = parser.SentStatus
+				result[tracking.ResultRelayNameKey] = "relay1.name"
+				result[tracking.ResultRelayIPKey] = "123.2.3.4"
+				result[tracking.ResultRelayPortKey] = 42
+				result[tracking.ResultDeliveryServerKey] = "server"
+				pub.Publish(result)
+			}
+
+			{
+				result := tracking.Result{}
+				result[tracking.ConnectionBeginKey] = 2
+				result[tracking.ConnectionEndKey] = 3
+				result[tracking.ConnectionClientHostnameKey] = "client.host"
+				result[tracking.ConnectionClientIPKey] = "127.0.0.1"
+				result[tracking.QueueBeginKey] = 2
+				result[tracking.QueueEndKey] = 3
+				result[tracking.QueueSenderLocalPartKey] = "sender2"
+				result[tracking.QueueSenderDomainPartKey] = "sender2.com"
+				result[tracking.QueueOriginalMessageSizeKey] = 32
+				result[tracking.QueueProcessedMessageSizeKey] = 80
+				result[tracking.QueueNRCPTKey] = 5
+				result[tracking.QueueMessageIDKey] = "lala@caca.com"
+				result[tracking.ResultDeliveryTimeKey] = 3
+				result[tracking.ResultRecipientLocalPartKey] = "recipient"
+				result[tracking.ResultRecipientDomainPartKey] = "recipient1.com"
+				result[tracking.ResultOrigRecipientLocalPartKey] = nil
+				result[tracking.ResultOrigRecipientDomainPartKey] = nil
+				result[tracking.ResultDelayKey] = ""
+				result[tracking.ResultDelaySMTPDKey] = 1.0
+				result[tracking.ResultDelayCleanupKey] = 2.0
+				result[tracking.ResultDelayQmgrKey] = 3.0
+				result[tracking.ResultDelaySMTPKey] = 4.0
+				result[tracking.ResultDSNKey] = "2.0.0"
+				result[tracking.ResultStatusKey] = parser.SentStatus
+				result[tracking.ResultRelayNameKey] = "relay1.name"
+				result[tracking.ResultRelayIPKey] = "123.2.3.4"
+				result[tracking.ResultRelayPortKey] = 42
+				result[tracking.ResultDeliveryServerKey] = "server"
+				pub.Publish(result)
+			}
+
+			cancel()
+			done()
+
+			So(db.Close(), ShouldBeNil)
+		})
+	})
+}
+
+func buildDefaultResult() tracking.Result {
+	result := tracking.Result{}
+	result[tracking.ConnectionBeginKey] = 2
+	result[tracking.ConnectionEndKey] = 3
+	result[tracking.ConnectionClientHostnameKey] = "client.host"
+	result[tracking.ConnectionClientIPKey] = "127.0.0.1"
+	result[tracking.QueueBeginKey] = 2
+	result[tracking.QueueEndKey] = 3
+	result[tracking.QueueSenderLocalPartKey] = "sender"
+	result[tracking.QueueSenderDomainPartKey] = "sender.com"
+	result[tracking.QueueOriginalMessageSizeKey] = 32
+	result[tracking.QueueProcessedMessageSizeKey] = 80
+	result[tracking.QueueNRCPTKey] = 5
+	result[tracking.QueueMessageIDKey] = "lala@caca.com"
+	result[tracking.ResultDeliveryTimeKey] = 3
+	result[tracking.ResultRecipientLocalPartKey] = nil
+	result[tracking.ResultRecipientDomainPartKey] = nil
+	result[tracking.ResultOrigRecipientLocalPartKey] = nil
+	result[tracking.ResultOrigRecipientDomainPartKey] = nil
+	result[tracking.ResultDelayKey] = 1.0
+	result[tracking.ResultDelaySMTPDKey] = 1.0
+	result[tracking.ResultDelayCleanupKey] = 2.0
+	result[tracking.ResultDelayQmgrKey] = 3.0
+	result[tracking.ResultDelaySMTPKey] = 4.0
+	result[tracking.ResultDSNKey] = "2.0.0"
+	result[tracking.ResultStatusKey] = parser.SentStatus
+	result[tracking.ResultRelayNameKey] = "relay1.name"
+	result[tracking.ResultRelayIPKey] = "123.2.3.4"
+	result[tracking.ResultRelayPortKey] = 42
+	result[tracking.ResultDeliveryServerKey] = "server"
+	return result
+}
+
+func parseTimeInterval(from, to string) data.TimeInterval {
+	interval, err := data.ParseTimeInterval(from, to, time.UTC)
+	if err != nil {
+		panic("pasring interval")
+	}
+	return interval
+}
+
+var (
+	dummyContext = context.Background()
+)
+
+func countByStatus(dashboard dashboard.Dashboard, status parser.SmtpStatus, interval data.TimeInterval) int {
+	v, err := dashboard.CountByStatus(dummyContext, status, interval)
+	So(err, ShouldBeNil)
+	return v
+}
+
+func topBusiestDomains(dashboard dashboard.Dashboard, interval data.TimeInterval) dashboard.Pairs {
+	pairs, err := dashboard.TopBusiestDomains(dummyContext, interval)
+	So(err, ShouldBeNil)
+	return pairs
+}
+
+func topBouncedDomains(dashboard dashboard.Dashboard, interval data.TimeInterval) dashboard.Pairs {
+	pairs, err := dashboard.TopBouncedDomains(dummyContext, interval)
+	So(err, ShouldBeNil)
+	return pairs
+}
+
+func topDeferredDomains(dashboard dashboard.Dashboard, interval data.TimeInterval) dashboard.Pairs {
+	pairs, err := dashboard.TopDeferredDomains(dummyContext, interval)
+	So(err, ShouldBeNil)
+	return pairs
+}
+
+func deliveryStatus(dashboard dashboard.Dashboard, interval data.TimeInterval) dashboard.Pairs {
+	pairs, err := dashboard.DeliveryStatus(dummyContext, interval)
+	So(err, ShouldBeNil)
+	return pairs
+}
+
+func TestEntriesInsertion(t *testing.T) {
+	Convey("LogInsertion", t, func() {
+		dir, clearDir := testutil.TempDir(t)
+		defer clearDir()
+
+		buildWs := func() (*DB, func() error, func(), tracking.ResultPublisher, dashboard.Dashboard, func()) {
+			db, err := New(dir)
+			So(err, ShouldBeNil)
+			done, cancel := db.Run()
+			pub := db.ResultsPublisher()
+			dashboard, err := dashboard.New(db.ReadConnection())
+			So(err, ShouldBeNil)
+
+			return db, done, cancel, pub, dashboard, func() {
+				So(dashboard.Close(), ShouldBeNil)
+				So(db.Close(), ShouldBeNil)
+			}
+		}
+
+		smtpStatusRecordWithRecipient := func(status parser.SmtpStatus, t time.Time, recipientLocalPart, recipientDomainPart string) tracking.Result {
+			r := buildDefaultResult()
+			r[tracking.ResultRecipientLocalPartKey] = recipientLocalPart
+			r[tracking.ResultRecipientDomainPartKey] = recipientDomainPart
+			r[tracking.ResultDeliveryTimeKey] = t.Unix()
+			r[tracking.ResultStatusKey] = status
+			return r
+		}
+
+		smtpStatusRecord := func(status parser.SmtpStatus, t time.Time) tracking.Result {
+			return smtpStatusRecordWithRecipient(status, t, "recipient", "test.com")
+		}
+
+		Convey("Inserting entries", func() {
+			Convey("Inserts nothing", func() {
+				db, done, cancel, _, dashboard, dtor := buildWs()
+				defer dtor()
+				cancel()
+				So(done(), ShouldBeNil)
+
+				interval := parseTimeInterval("1999-12-02", "2000-01-03")
+
+				So(db.HasLogs(), ShouldBeFalse)
+				So(countByStatus(dashboard, parser.BouncedStatus, interval), ShouldEqual, 0)
+				So(countByStatus(dashboard, parser.DeferredStatus, interval), ShouldEqual, 0)
+				So(countByStatus(dashboard, parser.SentStatus, interval), ShouldEqual, 0)
+			})
+
+			Convey("Inserts one log entry", func() {
+				db, done, cancel, pub, dashboard, dtor := buildWs()
+				defer dtor()
+
+				pub.Publish(smtpStatusRecord(parser.SentStatus, testutil.MustParseTime(`1999-12-02 13:10:10 +0000`)))
+				cancel()
+				So(done(), ShouldBeNil)
+
+				interval := parseTimeInterval("1999-12-01", "2000-01-03")
+
+				So(db.HasLogs(), ShouldBeTrue)
+				So(countByStatus(dashboard, parser.BouncedStatus, interval), ShouldEqual, 0)
+				So(countByStatus(dashboard, parser.DeferredStatus, interval), ShouldEqual, 0)
+				So(countByStatus(dashboard, parser.SentStatus, interval), ShouldEqual, 1)
+			})
+
+			Convey("Insert, reopen, insert", func() {
+				func() {
+					_, done, cancel, pub, _, dtor := buildWs()
+					defer dtor()
+
+					// this one is before the time interval
+					pub.Publish(smtpStatusRecord(parser.DeferredStatus, testutil.MustParseTime(`1999-11-02 13:10:10 +0000`)))
+
+					pub.Publish(smtpStatusRecord(parser.SentStatus, testutil.MustParseTime(`1999-12-02 13:10:10 +0000`)))
+					cancel()
+					So(done(), ShouldBeNil)
+				}()
+
+				// reopen workspace and add another log
+				db, done, cancel, pub, dashboard, dtor := buildWs()
+				defer dtor()
+
+				pub.Publish(smtpStatusRecord(parser.SentStatus, testutil.MustParseTime(`1999-12-04 13:10:10 +0000`)))
+				pub.Publish(smtpStatusRecord(parser.DeferredStatus, testutil.MustParseTime(`1999-12-04 13:10:10 +0000`)))
+
+				pub.Publish(smtpStatusRecord(parser.BouncedStatus, testutil.MustParseTime(`2000-03-10 13:10:10 +0000`)))
+
+				// this one is after the time interval
+				pub.Publish(smtpStatusRecord(parser.DeferredStatus, testutil.MustParseTime(`2000-05-02 13:10:10 +0000`)))
+
+				cancel()
+				So(done(), ShouldBeNil)
+
+				interval := parseTimeInterval("1999-12-02", "2000-03-11")
+
+				So(db.HasLogs(), ShouldBeTrue)
+
+				So(countByStatus(dashboard, parser.BouncedStatus, interval), ShouldEqual, 1)
+				So(countByStatus(dashboard, parser.DeferredStatus, interval), ShouldEqual, 1)
+				So(countByStatus(dashboard, parser.SentStatus, interval), ShouldEqual, 2)
+			})
+
+			t := func(year int, month time.Month, day, hour, minute, second int) time.Time {
+				return time.Date(year, month, day, hour, minute, second, 0, time.UTC)
+			}
+
+			Convey("Many different smtp status", func() {
+				_, done, cancel, pub, d, dtor := buildWs()
+				defer dtor()
+
+				interval := parseTimeInterval("1999-12-02", "2000-03-11")
+
+				{
+					s := parser.SentStatus
+					d := parser.DeferredStatus
+					b := parser.BouncedStatus
+
+					// Something before the interval
+					pub.Publish(smtpStatusRecordWithRecipient(s, t(1999, time.December, 1, 13, 0, 0), "recip", "domain"))
+
+					// Inside the interval
+					pub.Publish(smtpStatusRecordWithRecipient(s, t(1999, time.December, 2, 14, 1, 2), "r1", "ALALALA.COM"))
+					pub.Publish(smtpStatusRecordWithRecipient(b, t(1999, time.December, 2, 14, 1, 3), "r2", "abcdf.com"))
+					pub.Publish(smtpStatusRecordWithRecipient(b, t(1999, time.December, 2, 14, 1, 4), "r3", "alalala.com"))
+					pub.Publish(smtpStatusRecordWithRecipient(d, t(1999, time.December, 3, 14, 1, 4), "r3", "EMAIL2.COM"))
+					pub.Publish(smtpStatusRecordWithRecipient(d, t(1999, time.December, 5, 15, 1, 0), "r2", "email3.com"))
+					pub.Publish(smtpStatusRecordWithRecipient(b, t(1999, time.December, 6, 16, 1, 4), "r3", "ALALALA.COM"))
+					pub.Publish(smtpStatusRecordWithRecipient(b, t(2000, time.January, 3, 15, 1, 0), "r2", "abcdf.com"))
+					pub.Publish(smtpStatusRecordWithRecipient(d, t(2000, time.January, 4, 15, 1, 0), "r2", "EMAIL1.COM"))
+					pub.Publish(smtpStatusRecordWithRecipient(s, t(2000, time.January, 4, 16, 1, 0), "r2", "example1.com"))
+					pub.Publish(smtpStatusRecordWithRecipient(s, t(2000, time.January, 4, 16, 2, 1), "r2", "example1.com"))
+					pub.Publish(smtpStatusRecordWithRecipient(b, t(2000, time.March, 11, 16, 2, 1), "r100", "email2.com"))
+
+					// Something after the interval
+					pub.Publish(smtpStatusRecordWithRecipient(d, t(2000, time.March, 12, 13, 0, 0), "recip", "domain"))
+				}
+
+				cancel()
+				So(done(), ShouldBeNil)
+
+				Convey("Busiest: used domain, regardless of the status", func() {
+					So(topBusiestDomains(d, interval), ShouldResemble, dashboard.Pairs{
+						dashboard.Pair{Key: "alalala.com", Value: 3},
+						dashboard.Pair{Key: "abcdf.com", Value: 2},
+						dashboard.Pair{Key: "email2.com", Value: 2},
+						dashboard.Pair{Key: "example1.com", Value: 2},
+						dashboard.Pair{Key: "email1.com", Value: 1},
+						dashboard.Pair{Key: "email3.com", Value: 1},
+					})
+				})
+
+				Convey("Bounced: status = bounced", func() {
+					So(topBouncedDomains(d, interval), ShouldResemble, dashboard.Pairs{
+						dashboard.Pair{Key: "abcdf.com", Value: 2},
+						dashboard.Pair{Key: "alalala.com", Value: 2},
+						dashboard.Pair{Key: "email2.com", Value: 1},
+					})
+				})
+
+				Convey("Deferred: status = deferred", func() {
+					So(topDeferredDomains(d, interval), ShouldResemble, dashboard.Pairs{
+						dashboard.Pair{Key: "email1.com", Value: 1},
+						dashboard.Pair{Key: "email2.com", Value: 1},
+						dashboard.Pair{Key: "email3.com", Value: 1},
+					})
+				})
+
+				Convey("Delivery Status", func() {
+					So(deliveryStatus(d, interval), ShouldResemble, dashboard.Pairs{
+						dashboard.Pair{Key: "sent", Value: 3},
+						dashboard.Pair{Key: "bounced", Value: 5},
+						dashboard.Pair{Key: "deferred", Value: 3},
+					})
+				})
+			})
+
+			Convey("Group According to Domain mapping", func() {
+				_, done, cancel, pub, d, dtor := buildWs()
+				defer dtor()
+
+				{
+					s := parser.SentStatus
+					d := parser.DeferredStatus
+					b := parser.BouncedStatus
+
+					pub.Publish(smtpStatusRecordWithRecipient(d, t(2020, time.January, 1, 1, 0, 0), "p1", "domaintobegrouped.de"))
+					pub.Publish(smtpStatusRecordWithRecipient(d, t(2020, time.January, 2, 1, 0, 0), "p1", "another.de"))
+					pub.Publish(smtpStatusRecordWithRecipient(d, t(2020, time.January, 2, 2, 0, 0), "p1", "domaintobegrouped.com"))
+
+					pub.Publish(smtpStatusRecordWithRecipient(b, t(2020, time.January, 3, 1, 0, 0), "p1", "domaintobegrouped.de"))
+					pub.Publish(smtpStatusRecordWithRecipient(b, t(2020, time.January, 4, 1, 0, 0), "p1", "domaintobegrouped.com"))
+					pub.Publish(smtpStatusRecordWithRecipient(b, t(2020, time.January, 5, 1, 0, 0), "p1", "domaintobegrouped.de"))
+					pub.Publish(smtpStatusRecordWithRecipient(b, t(2020, time.January, 6, 1, 0, 0), "p1", "another.de"))
+
+					pub.Publish(smtpStatusRecordWithRecipient(s, t(2020, time.January, 6, 1, 0, 0), "p1", "domaintobegrouped.com"))
+				}
+
+				cancel()
+				So(done(), ShouldBeNil)
+
+				interval := parseTimeInterval(`2020-01-01`, `2020-12-31`)
+
+				So(topBusiestDomains(d, interval), ShouldResemble, dashboard.Pairs{
+					dashboard.Pair{Key: "grouped", Value: 6},
+					dashboard.Pair{Key: "another.de", Value: 2},
+				})
+
+				So(topBouncedDomains(d, interval), ShouldResemble, dashboard.Pairs{
+					dashboard.Pair{Key: "grouped", Value: 3},
+					dashboard.Pair{Key: "another.de", Value: 1},
+				})
+
+				So(topDeferredDomains(d, interval), ShouldResemble, dashboard.Pairs{
+					dashboard.Pair{Key: "grouped", Value: 2},
+					dashboard.Pair{Key: "another.de", Value: 1},
+				})
+			})
+		})
+	})
+}
---
+++ deliverydb/migrations/1_delivery_tables.go
@@ -0,0 +1,82 @@
+package migrations
+
+import (
+	"database/sql"
+	"gitlab.com/lightmeter/controlcenter/lmsqlite3/migrator"
+	"gitlab.com/lightmeter/controlcenter/util/errorutil"
+)
+
+func init() {
+	migrator.AddMigration("deliverydb", "1_delivery_tables.go", up, down)
+}
+
+func up(tx *sql.Tx) error {
+	// TODO: investigate, via profiling, which fields deserve to have indexes apart from the obvious ones.
+	sql := `
+create table deliveries (
+	status integer not null,
+	delivery_ts integer not null,
+	sender_domain_part_id integer not null,
+	recipient_domain_part_id integer not null,
+	orig_recipient_domain_part_id integer, -- optional
+	message_id integer not null,
+	conn_ts_begin integer, -- FIXME: due a parser issue with NOQUEUE, we sometimes don't have this value :-(
+	queue_ts_begin integer not null,
+	orig_msg_size integer not null,
+	processed_msg_size integer not null,
+	nrcpt integer not null,
+	delivery_server_id integer not null,
+	delay double not null,
+	delay_smtpd double not null,
+	delay_cleanup double not null,
+	delay_qmgr double not null,
+	delay_smtp double not null,
+	next_relay_id integer, -- optional
+	sender_local_part text not null,
+	recipient_local_part text not null,
+	orig_recipient_local_part text,
+	client_hostname text, -- FIXME: due a parser issue with NOQUEUE, we sometimes don't have this value :-(
+	client_ip blob, -- FIXME: due a parser issue with NOQUEUE, we sometimes don't have this value :-(
+	dsn text not null
+);
+
+create index deliveries_ts_index on deliveries(delivery_ts);
+create index deliveries_status_delivery_ts_index on deliveries(status, delivery_ts);
+
+create table messageids (
+	value text not null
+);
+
+create index messageids_index on messageids(value);
+
+create table remote_domains (
+	domain text not null
+);
+
+create index remote_domains_index on remote_domains(domain);
+
+create table next_relays (
+	port integer not null,
+	hostname text not null,
+	ip blob not null
+);
+
+create index next_relays_index on next_relays(hostname, ip, port);
+
+create table delivery_server (
+	hostname text not null
+);
+
+create index delivery_server_hostname_index on delivery_server(hostname);
+
+`
+	if _, err := tx.Exec(sql); err != nil {
+		return errorutil.Wrap(err)
+	}
+
+	return nil
+}
+
+func down(tx *sql.Tx) error {
+	return nil
+}
