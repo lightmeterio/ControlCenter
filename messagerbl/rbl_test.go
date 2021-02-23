9dca3372ffbcc707704ecc50c5e9a6465b709b52
Leandro Santiago <leandro@lightmeter.io>
2021-02-23 16:46:52 +0100

    Detect another Microsoft RBL from logs
    Ref #24
    
    Those logs were found on our mail server

--- messagerbl/rbl_test.go
+++ messagerbl/rbl_test.go
@@ -53,6 +53,8 @@ func TestRBL(t *testing.T) {
 			record(`Jan 21 14:50:00 mail postfix-135.97.192.135/smtp[25870]: 333E52C4FF72: to=<a.recip@example.com>, relay=us-smtp-inbound-2.mimecast.com[11.22.33.44]:25, delay=1.5, delays=0/0/1.1/0.36, dsn=5.0.0, status=bounced (host us-smtp-inbound-2.mimecast.com[11.22.33.44] said: 550 csi.mimecast.org Poor Reputation Sender. - https://community.mimecast.com/docs/DOC-1369#550 [EM-f6hnWN9maa42OLPquOA.us166] (in reply to RCPT TO command))`),
 			// no match, no pattern found
 			record(`Jan 22 06:01:00 smtpnode51 postfix-135.97.192.135/smtp[5743]: 4B0D5410DE: to=<h-d66579f3@h-83e81c.com>, relay=h-50f3c4ccac5be32ede8[38.170.83.23]:25, delay=2000, delays=1999/0.13/1.1/0, dsn=4.7.0, status=deferred (host h-50f3c4ccac5be32ede8[38.170.83.23] refused to talk to me: 550 5.7.0 Blocked - see https://h-da21bcc5400cebb4aa45f2/h-fe4ec82fb61b5c4c?ip=135.97.192.135)`),
+			// Microsoft
+			record(`Feb 18 11:55:37 mail postfix/smtp[19793]: 6BAB0300C9CD: to=<someone@hotmail.com>, relay=hotmail-com.olc.protection.outlook.com[104.47.66.33]:25, delay=0.7, delays=0.04/0.02/0.47/0.16, dsn=5.7.1, status=bounced (host hotmail-com.olc.protection.outlook.com[104.47.66.33] said: 550 5.7.1 Unfortunately, messages from [1.2.3.4] weren't sent. Please contact your Internet service provider since part of their network is on our block list (S3140). You can also refer your provider to http://mail.live.com/mail/troubleshooting.aspx#errors. [MW2NAM12FT005.eop-nam12.prod.protection.outlook.com] (in reply to MAIL FROM command))`),
 		}
 
 		for _, r := range records {
@@ -76,7 +78,7 @@ func TestRBL(t *testing.T) {
 
 		results := <-resultsChan
 
-		So(len(results), ShouldEqual, 2)
+		So(len(results), ShouldEqual, 3)
 
 		So(results[0], ShouldResemble, Result{
 			Host:    "Microsoft",
@@ -94,5 +96,12 @@ func TestRBL(t *testing.T) {
 			Time:    records[2].Time,
 		})
 
+		So(results[2], ShouldResemble, Result{
+			Host:    "Microsoft",
+			Address: ip,
+			Payload: records[4].Payload.(parser.SmtpSentStatus),
+			Header:  records[4].Header,
+			Time:    records[4].Time,
+		})
 	})
 }
