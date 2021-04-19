7becc840634653546e8e1f04e998b6a569be6169
Leandro Santiago <leandro@lightmeter.io>
2021-04-19 10:01:51 +0200

    Use gitlab package registry to store linux amd64 builds
    Ref #389
    
    Also compute sha256sum for it

--- ci/release_on_gitlab.sh
+++ ci/release_on_gitlab.sh
@@ -12,11 +12,30 @@ if [ ! -e "$RELEASE_NOTES" ]; then
   exit 1
 fi
 
-# part of image registry.gitlab.com/gitlab-org/release-cli
+ARTIFACT="lightmeter-linux_amd64-$VERSION"
+CHECKSUM=sha256.txt
+
+apk add curl
+
+cp lightmeter "$ARTIFACT"
+
+sha256sum "$ARTIFACT" >> "$CHECKSUM"
+
+ARTIFACT_URL="${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/generic/lightmeter/$VERSION/$ARTIFACT"
+CHECKSUM_URL="${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/generic/lightmeter/$VERSION/$CHECKSUM"
+
+# first, upload stuff to the package registry
+curl --header "JOB-TOKEN: $CI_JOB_TOKEN" --upload-file "$ARTIFACT" "$ARTIFACT_URL"
+curl --header "JOB-TOKEN: $CI_JOB_TOKEN" --upload-file "$CHECKSUM" "$CHECKSUM_URL"
+
+# and finally perform the release
+
 release-cli create \
   --name "ControlCenter $VERSION" \
   --description "$(cat $RELEASE_NOTES)" \
   --tag-name "release/$VERSION" \
   --ref $CI_COMMIT_SHA \
-  --assets-links-name "Binary for linux amd64" \
-  --assets-links-url "https://bintray.com/lightmeter/controlcenter/download_file?file_path=lightmeter-linux_amd64-$VERSION"
+  --assets-links-name "Binary for Linux amd64" \
+  --assets-links-url "$ARTIFACT_URL" \
+  --assets-links-name "Binary for Linux amd64 (sha256sum)" \
+  --assets-links-url "$CHECKSUM_URL"
