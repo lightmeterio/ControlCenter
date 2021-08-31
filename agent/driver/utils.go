e25311935268c6d664e681314b43d2709f9d08d4
Leandro Santiago <leandro@lightmeter.io>
2021-08-31 09:40:31 +0200

    Small fixes on agent code after review

--- agent/driver/utils.go
+++ agent/driver/utils.go
@@ -1,8 +1,8 @@
-package driver
-
 // SPDX-FileCopyrightText: 2021 Lightmeter <hello@lightmeter.io>
 //
 // SPDX-License-Identifier: AGPL-3.0-only
+
+package driver
 
 import (
 	"context"
@@ -17,7 +17,6 @@ var (
 )
 
 func ReadFileContent(ctx context.Context, driver Driver, filepath string, dst io.Writer) error {
-	// then get the content
 	if err := driver.ExecuteCommand(context.Background(), []string{"cat", filepath}, nil, dst, Stderr); err != nil {
 		return errorutil.Wrap(err)
 	}
@@ -26,7 +25,7 @@ func ReadFileContent(ctx context.Context, driver Driver, filepath string, dst io
 }
 
 func WriteFileContent(ctx context.Context, driver Driver, filepath string, src io.Reader) error {
-	catCommand := fmt.Sprintf(`cat > %s`, filepath)
+	catCommand := fmt.Sprintf(`cat > "%s"`, filepath)
 
 	if err := driver.ExecuteCommand(context.Background(), []string{`sh`, `-c`, catCommand}, src, Stdout, Stderr); err != nil {
 		return errorutil.Wrap(err)
