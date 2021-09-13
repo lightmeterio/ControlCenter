98996076c51019192f9b77e59ee50bdf654e24b5
Leandro Santiago <leandro@lightmeter.io>
2021-09-13 10:38:46 +0200

    Make deliverydb not try to recover from panic on non-release code
    Ref #204
    
    The idea is to make it easier to fix the existing bugs during development

---
+++ deliverydb/recover_release.go
@@ -0,0 +1,22 @@
+// SPDX-FileCopyrightText: 2021 Lightmeter <hello@lightmeter.io>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+//go:build release
+// +build release
+
+package deliverydb
+
+import (
+	"github.com/rs/zerolog/log"
+	"gitlab.com/lightmeter/controlcenter/tracking"
+)
+
+func recoverFromError(err *error, tr tracking.Result) {
+	if r := recover(); r != nil {
+		log.Error().Object("result", tr).Msg("Failed to store delivery message")
+
+		// FIXME: horrendous workaround while we cannot figure out the cause of the issue!
+		*err = nil
+	}
+}
