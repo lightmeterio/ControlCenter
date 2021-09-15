8c5ac7ef92786cfcac88545a85d00a6b238be2b4
Leandro Santiago <leandro@lightmeter.io>
2021-09-15 18:41:58 +0200

    Remove old intel reports from time to time to prevent database of growing indefinitely
    Ref #204

--- intel/collector/collector.go
+++ intel/collector/collector.go
@@ -127,11 +127,6 @@ func New(intelDb *dbconn.PooledPair, options Options, reporters Reporters, dispa
 	return NewWithCustomClock(intelDb, options, reporters, dispatcher, &timeutil.RealClock{})
 }
 
-func oldEntriesCleaner(tx *sql.Tx, stmts dbconn.TxPreparedStmts) error {
-	// TODO: implement it!
-	return nil
-}
-
 // NOTE: New takes ownwership of the reporters, calling Close() when it ends
 func NewWithCustomClock(pair *dbconn.PooledPair, options Options, reporters Reporters, dispatcher Dispatcher, clock timeutil.Clock) (*Collector, error) {
 	closers := closeutil.New()
@@ -142,11 +137,14 @@ func NewWithCustomClock(pair *dbconn.PooledPair, options Options, reporters Repo
 
 	stmts := dbconn.PreparedStmts{}
 
+	// ~3 months. TODO: make it configurable
+	const maxAge = (time.Hour * 24 * 30 * 3)
+
 	return &Collector{
 		reporters: reporters,
 		Closers:   closers,
 		CancellableRunner: runner.NewCancellableRunner(func(done runner.DoneChan, cancel runner.CancelChan) {
-			dbRunner := dbrunner.New(options.CycleInterval, 10, pair, stmts, time.Hour*12, oldEntriesCleaner)
+			dbRunner := dbrunner.New(options.CycleInterval, 10, pair, stmts, time.Hour*12, makeCleanAction(maxAge))
 			dbRunnerDone, dbRunnerCancel := runner.Run(dbRunner)
 
 			go func() {
@@ -175,4 +173,27 @@ func NewWithCustomClock(pair *dbconn.PooledPair, options Options, reporters Repo
 			}()
 		}),
 	}, nil
+}
+
+func makeCleanAction(maxAge time.Duration) dbrunner.Action {
+	return func(tx *sql.Tx, stmts dbconn.TxPreparedStmts) error {
+		var mostRecentDispatchTime int64
+		if err := tx.QueryRow(`select time from queued_reports order by id desc limit 1`).Scan(&mostRecentDispatchTime); err != nil {
+			return errorutil.Wrap(err)
+		}
+
+		mostRecentTime := time.Unix(mostRecentDispatchTime, 0)
+		oldestTimeToKeep := mostRecentTime.Add(-maxAge)
+		oldestTimeToKeepInTimestamp := oldestTimeToKeep.Unix()
+
+		if _, err := tx.Exec(`delete from queued_reports where time < ?`, oldestTimeToKeepInTimestamp); err != nil {
+			return errorutil.Wrap(err)
+		}
+
+		if _, err := tx.Exec(`delete from dispatch_times where time < ?`, oldestTimeToKeepInTimestamp); err != nil {
+			return errorutil.Wrap(err)
+		}
+
+		return nil
+	}
 }
