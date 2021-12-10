c8d26aefea20a9cd1e8a9feeda8acc0a368e2591
Leandro Santiago <leandro@lightmeter.io>
2021-12-10 21:50:16 +0100

    Fix rebase artifact

--- intel/intel.go
+++ intel/intel.go
@@ -292,7 +292,7 @@ func DefaultVersionBuilder() Version {
 
 func New(intelDb *dbconn.PooledPair, deliveryDbPool *dbconn.RoPool, fetcher insightscore.Fetcher,
 	settingsReader metadata.Reader, auth auth.Registrar, connStatsPool *dbconn.RoPool,
-	options Options) (*Runner, *logslinecount.Publisher, bruteforce.Checker, error) {
+	options Options) (*Runner, *logslinecount.Publisher, blockedips.Checker, error) {
 	logslinePublisher := logslinecount.NewPublisher()
 
 	reporters := collector.Reporters{
