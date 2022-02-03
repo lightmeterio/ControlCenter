c0c1b318c50f99b5afa8b92365fcf85056c5df04
Leandro Santiago <leandro@lightmeter.io>
2022-02-03 19:22:39 -0300

    Reuse io.Closer in insights

--- insights/core/core.go
+++ insights/core/core.go
@@ -10,13 +10,14 @@ import (
 	notificationCore "gitlab.com/lightmeter/controlcenter/notification/core"
 	"gitlab.com/lightmeter/controlcenter/pkg/closers"
 	"gitlab.com/lightmeter/controlcenter/util/timeutil"
+	"io"
 )
 
 type Clock = timeutil.Clock
 
 type Detector interface {
+	io.Closer
 	Step(Clock, *sql.Tx) error
-	Close() error
 }
 
 type HistoricalDetector interface {
