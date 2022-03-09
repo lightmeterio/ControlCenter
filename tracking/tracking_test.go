b9edfedcf343dfa65bdff9f3e421d0113389cb7a
Leandro Santiago <leandro@lightmeter.io>
2022-03-09 17:55:21 +0000

    Fix local deliveries not being recognized as inbound traffic

--- tracking/tracking_test.go
+++ tracking/tracking_test.go
@@ -616,6 +616,20 @@ func TestTrackingFromFiles(t *testing.T) {
 					So(pub.results[0][ResultMessageDirectionKey].Int64(), ShouldEqual, MessageDirectionIncoming)
 				})
 
+				Convey("Received bounced message is inbound", func() {
+					readFromTestFile("../test_files/postfix_logs/individual_files/28_received_bounce.log", t.Publisher())
+					cancel()
+					done()
+
+					So(len(pub.results), ShouldEqual, 1)
+
+					So(pub.results[0][QueueSenderDomainPartKey].Text(), ShouldEqual, "microsoft.onmicrosoft.com")
+					So(pub.results[0][QueueSenderLocalPartKey].Text(), ShouldEqual, "bounces+YYY=76YYY=S7")
+					So(pub.results[0][ResultRecipientDomainPartKey].Text(), ShouldEqual, "example.com")
+					So(pub.results[0][ResultRecipientLocalPartKey].Text(), ShouldEqual, "root")
+					So(pub.results[0][ResultMessageDirectionKey].Int64(), ShouldEqual, MessageDirectionIncoming)
+				})
+
 				Convey("authcleanup/cleanup (Gitlab issue #621)", func() {
 					postfixutil.ReadFromTestFileWithFormat("../test_files/postfix_logs/individual_files/25_authclean_cleanup.log", t.Publisher(), 2021, "rfc3339", &timeutil.FakeClock{Time: timeutil.MustParseTime(`2021-12-31 00:00:00 +0000`)})
 					cancel()
