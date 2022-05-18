bd6d694aa65818d3097450411e988f1d882c4883
Leandro Santiago <leandro@lightmeter.io>
2022-05-18 10:30:00 +0200

    Fix getting public url for session
    Ref #440

--- server/server.go
+++ server/server.go
@@ -19,6 +19,7 @@ import (
 	"gitlab.com/lightmeter/controlcenter/httpmiddleware"
 	"gitlab.com/lightmeter/controlcenter/httpsettings"
 	"gitlab.com/lightmeter/controlcenter/i18n"
+	"gitlab.com/lightmeter/controlcenter/metadata"
 	"gitlab.com/lightmeter/controlcenter/newsletter"
 	"gitlab.com/lightmeter/controlcenter/po"
 	"gitlab.com/lightmeter/controlcenter/settings"
@@ -60,18 +61,7 @@ func (s *HttpServer) Start() error {
 
 	setup := httpsettings.NewSettings(writer, reader, initialSetupSettings, s.Workspace.NotificationCenter, s.Workspace.InsightsEngine())
 
-	publicURL, err := func() (*url.URL, error) {
-		globSettings, err := globalsettings.GetSettings(context.Background(), reader)
-		if err != nil {
-			return nil, errorutil.Wrap(err)
-		}
-
-		if globSettings.PublicURL == "" {
-			return nil, nil
-		}
-
-		return url.Parse(globSettings.PublicURL)
-	}()
+	publicURL, err := getPublicURL(context.Background(), reader)
 
 	if err != nil {
 		return errorutil.Wrap(err)
@@ -116,4 +106,21 @@ func (s *HttpServer) Start() error {
 	log.Info().Msgf("Lightmeter ControlCenter is running on http://%s", ln.Addr().String())
 
 	return server.Serve(ln)
+}
+
+func getPublicURL(ctx context.Context, reader metadata.Reader) (*url.URL, error) {
+	globSettings, err := globalsettings.GetSettings(ctx, reader)
+	if err != nil && errors.Is(err, metadata.ErrNoSuchKey) {
+		return nil, nil
+	}
+
+	if err != nil {
+		return nil, errorutil.Wrap(err)
+	}
+
+	if globSettings.PublicURL == "" {
+		return nil, nil
+	}
+
+	return url.Parse(globSettings.PublicURL)
 }
